59da214c84bfdcc93b91d372a7f6e614af19d3c1
Yellathkota-Jaswanth <jaswanthyellathkota@gmail.com>
2024-11-09 21:14:12 +0530

    Delete client/Liv directory

--- client/Liv/src/utils/http.js
+++
@@ -1,9 +0,0 @@
-// utils/http.js
-import axios from 'axios';
-
-const http = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust if needed
-  timeout: 5000, // Increase timeout if requests take longer
-});
-
-export default http;
